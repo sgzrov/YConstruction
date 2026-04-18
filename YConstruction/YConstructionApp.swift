ac5d4ee5327e2cf01e265ed670e422ee8c493bab
Muhammadjon Marufov <marufov254@gmail.com>
2026-04-18 15:08:25 -0700

    Mmarufov/hk tour plan (#1)
    * feat: add YConstruction iOS Xcode project
    
    * chore: add .gitignore, remove Xcode user data from tracking

---
+++ YConstruction/YConstructionApp.swift
@@ -0,0 +1,17 @@
+//
+//  YConstructionApp.swift
+//  YConstruction
+//
+//  Created by Muhammad Marufov on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct YConstructionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
